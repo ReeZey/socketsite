fc6a8cc5be650729c6dcdbc57753b142ae5b7c03
ReeZey <johannes@seved.net>
2023-08-10 23:55:51 +0200

    chat works

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 use std::collections::HashMap;
 
 use http::handle_http;
-use socket::handle_socket;
-use tokio::{net::{TcpListener, TcpStream}, io::{BufReader, AsyncBufReadExt}};
+use socket::{handle_socket, Message};
+use tokio::{net::{TcpListener, TcpStream}, io::{BufReader, AsyncBufReadExt}, sync::broadcast::{channel, Sender, Receiver}};
+
+use uuid::Uuid;
 
 mod http;
 mod socket;
 
 #[tokio::main]
 async fn main() {
-    let server = TcpListener::bind("0.0.0.0:80").await.unwrap();
+    let server = TcpListener::bind("0.0.0.0:51413").await.unwrap();
+
+    let (sender, _) = channel(10);
 
     loop {
         let (stream, _socket_addr) = server.accept().await.unwrap();
 
+        let local_sender = sender.clone();
+        let local_receiver = sender.subscribe();
+
         tokio::spawn(async move {
-            handle_connection(stream).await;
+            handle_connection(stream, local_sender, local_receiver).await;
         });
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream, sender: Sender<Message>, receiver: Receiver<Message>) {
     let buf_reader = BufReader::new(&mut stream);
     let mut lines = buf_reader.lines();
     let inital = lines.next_line().await.unwrap().unwrap();
@@ -39,11 +46,18 @@ async fn handle_connection(mut stream: TcpStream) {
     if let Some(upgrade_type) = headers.get("Upgrade") {
         if upgrade_type == "websocket" {
             //println!("socket");
+
             println!("> {:?}", stream.peer_addr().unwrap());
-            handle_socket(&mut stream, inital, headers).await;
+            handle_socket(stream, inital, headers, sender, receiver).await;
             return;
         }
     }
     //println!("http");
     handle_http(&mut stream, inital, headers).await;
+}
+
+#[derive(Debug, Clone)]
+pub struct User {
+    pub uuid: Uuid,
+    pub name: String
 }
--- src/socket.rs
+++ src/socket.rs
@@ -1,14 +1,15 @@
-use std::collections::HashMap;
-use tokio::{net::TcpStream, io::AsyncWriteExt};
+use std::{collections::HashMap, sync::Arc};
+use tokio::{net::{TcpStream, tcp::OwnedReadHalf}, io::AsyncWriteExt, sync::{broadcast::{Sender, Receiver}, Mutex}};
 use base64::{Engine as _, engine::general_purpose};
 use tokio::io::AsyncReadExt;
 use std::io::Write;
 use rand::prelude::*;
+use uuid::Uuid;
 
 const MAGIC_HASH: &str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
-pub async fn handle_socket(mut stream: &mut TcpStream, _inital: String, headers: HashMap<String, String>) {
-    let key = headers.get("Sec-WebSocket-Key").unwrap();
+pub async fn handle_socket(mut stream: TcpStream, _inital: String, headers: HashMap<String, String>, sender: Sender<Message>, mut receiver: Receiver<Message>) {
+    let mut key: String = headers.get("Sec-WebSocket-Key").unwrap().to_owned();
 
     let to_be_hashed = format!("{}{}", key, MAGIC_HASH);
 
@@ -27,45 +28,174 @@ pub async fn handle_socket(mut stream: &mut TcpStream, _inital: String, headers:
     write!(&mut buffer, "\r\n").unwrap();
     stream.write(&mut buffer).await.unwrap();
 
+    //let frame_out = FrameUtils::create(1,true, b"connected!".to_vec(), Some(FrameUtils::random_mask()));
+    //stream.write(&frame_out).await.unwrap();
+
+    let uuid = Uuid::new_v4();
+    let (mut read_stream, mut write_stream) = stream.into_split();
+    
+    sender.send(Message::connect(&key, uuid)).unwrap();
+    sender.send(Message::only_self(&format!("connected! you are {:?}", key), uuid)).unwrap();
+
+    let alive = Arc::new(Mutex::new(true));
+    let recv_alive = alive.clone();
+
+    tokio::spawn(async move {
+        while *recv_alive.lock().await {
+            let message = receiver.recv().await.unwrap();
+
+            match message.msg_type {
+                MessageType::Connect => {
+                    if message.uuid != uuid {
+                        let frame_out = FrameUtils::create(1, true, format!("{} joined", message.text.unwrap()).as_bytes().to_vec(), Some(FrameUtils::random_mask()));
+                        write_stream.write(&frame_out).await.unwrap();
+                    }
+                },
+                MessageType::Disconnect => {
+                    if message.uuid != uuid {
+                        let frame_out = FrameUtils::create(1, true, format!("{} left", message.text.unwrap()).as_bytes().to_vec(), Some(FrameUtils::random_mask()));
+                        write_stream.write(&frame_out).await.unwrap();
+                    }
+                },
+                MessageType::Message => {
+                    if message.uuid != uuid {
+                        let frame_out = FrameUtils::create(1, true, message.text.unwrap().as_bytes().to_vec(), Some(FrameUtils::random_mask()));
+                        write_stream.write(&frame_out).await.unwrap();
+                    }
+                },
+                MessageType::OnlySelf => {
+                    if message.uuid == uuid {
+                        let frame_out = FrameUtils::create(1, true, message.text.unwrap().as_bytes().to_vec(), Some(FrameUtils::random_mask()));
+                        write_stream.write(&frame_out).await.unwrap();
+                    }
+                },
+            }
+        }
+    });
+
+    let mut first = true;
     let mut data = vec![];
     loop {
-        let frame = FrameUtils::read(&mut stream).await;
+        let frame = FrameUtils::read(&mut read_stream).await;
+
         data.extend(frame.data);
         if frame.finished {
-            //println!("opcode: {:X} - data: {:X?}", frame.opcode, data);
             match frame.opcode {
                 0 => {
                     continue;
                 }
                 1 => {
-                    let message = String::from_utf8(data.clone()).unwrap();
+                    let mut message = String::from_utf8(data.clone()).unwrap();
 
-                    if message == "hello" {
-                        let frame_out = FrameUtils::create(1,true, b"hello".to_vec(), Some(FrameUtils::random_mask()));
-                        stream.write(&frame_out).await.unwrap();
+                    if first {
+                        if message != "0.0.13" {
+                            sender.send(Message::only_self("reload", uuid)).unwrap();
+                        }
+                        first = false;
+                        data.clear();
+                        continue;
                     }
 
-                    println!("message: {}", message);
+                    if message.starts_with("/") {
+                        message.remove(0);
+
+                        let (command, args) = match message.split_once(" ") {
+                            Some((command, args)) => {
+                                (command, Some(args))
+                            },
+                            None => {
+                                (message.as_str(), None)
+                            },
+                        };
+
+                        match command {
+                            "rename" => 'yeet: {
+                                if args.is_none() {
+                                    sender.send(Message::only_self("SERVER: you need to send a username too noob", uuid)).unwrap();
+                                    break 'yeet;
+                                }
+
+                                let old = key.clone();
+
+                                key = args.unwrap().to_owned();
+
+                                sender.send(Message::only_self(&format!("SERVER: you are now {:?}", key), uuid)).unwrap();
+                                sender.send(Message::message(&format!("SERVER: {:?} became {:?}", old, key), uuid)).unwrap();
+                            },
+                            _ => {
+                                sender.send(Message::only_self("SERVER: what what now?", uuid)).unwrap();
+                            }
+                        };
+                        data.clear();
+                        continue;
+                    }
+
+                    let message = message.trim();
+                    println!("{} [{}]: {}", read_stream.peer_addr().unwrap().to_string(), key, message);
+                    sender.send(Message::message(&format!("{}: {}", key, message), uuid)).unwrap();
+                    sender.send(Message::only_self(&format!("you: {}", message), uuid)).unwrap();
                 }
                 2 => {
-                    println!("binary: {:X?}", data.clone());
+                    //println!("binary: {:X?}", data.clone());
+                    //break;
                 }
                 8 => {
                     println!("socket gone 🦀");
-                    return;
+                    sender.send(Message::disconnect(&key, uuid)).unwrap();
+                    break;
                 }
                 _ => {}
             }
-            /*
-            //ping example
-            thread::sleep(Duration::from_secs(5));
-
-            let frame_out = FrameUtils::create(9, true, vec![0,1,2,3], Some(FrameUtils::random_mask()));
-            stream.write(&frame_out).await.unwrap();
-            */
             data.clear();
         }
     }
+    *alive.lock().await = false;
+}
+
+#[derive(Debug, Clone)]
+pub struct Message {
+    pub text: Option<String>,
+    pub uuid: Uuid,
+    pub msg_type: MessageType
+}
+
+impl Message {
+    fn message(text: &str, uuid: Uuid) -> Self {
+        Self {
+            text: Some(text.to_owned()),
+            uuid,
+            msg_type: MessageType::Message
+        }
+    }
+    fn only_self(text: &str, uuid: Uuid) -> Self {
+        Self {
+            text: Some(text.to_owned()),
+            uuid,
+            msg_type: MessageType::OnlySelf
+        }
+    }
+    fn disconnect(text: &str, uuid: Uuid) -> Self {
+        Self {
+            text: Some(text.to_owned()),
+            uuid,
+            msg_type: MessageType::Disconnect
+        }
+    }
+    fn connect(text: &str, uuid: Uuid) -> Self {
+        Self {
+            text: Some(text.to_owned()),
+            uuid,
+            msg_type: MessageType::Connect
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum MessageType {
+    Connect,
+    Disconnect,
+    Message,
+    OnlySelf
 }
 
 struct FrameUtils {
@@ -111,7 +241,8 @@ impl FrameUtils {
 
         return buffer;
     }
-    async fn read(stream: &mut TcpStream) -> Self {
+
+    async fn read(stream: &mut OwnedReadHalf) -> Self {
         let first_byte = stream.read_u8().await.unwrap();
         let secound_byte = stream.read_u8().await.unwrap();
 
@@ -153,6 +284,52 @@ impl FrameUtils {
             data,
         }
     }
+    /*
+    async fn read_buf(stream: Vec<u8>) -> Self {
+        let mut stream = Cursor::new(stream);
+
+        let first_byte = stream.read_u8().await.unwrap();
+        let secound_byte = stream.read_u8().await.unwrap();
+
+        let mut msg_len = (secound_byte & 0b01111111) as u64;
+
+        if msg_len == 126 {
+            msg_len = stream.read_u16().await.unwrap() as u64;
+        }
+
+        if msg_len == 127 {
+            msg_len = stream.read_u64().await.unwrap();
+        }
+
+        let masked = (secound_byte & 0b10000000) != 0;
+
+        let mask = match masked {
+            true => {
+                [
+                    stream.read_u8().await.unwrap(), 
+                    stream.read_u8().await.unwrap(), 
+                    stream.read_u8().await.unwrap(), 
+                    stream.read_u8().await.unwrap()
+                ]
+            }
+            false => {
+                [ 0, 0, 0, 0 ]
+            }
+        };
+
+        let mut data: Vec<u8> = vec![];
+
+        for i in 0..msg_len as usize {
+            data.push(stream.read_u8().await.unwrap() ^ mask[i % 4]);
+        }
+
+        Self {
+            opcode: (first_byte & 0b00001111),
+            finished: (first_byte & 0b10000000) != 0,
+            data,
+        }
+    }
+    */
     fn random_mask() -> [u8; 4] {
         let mut rng = rand::thread_rng();
 
